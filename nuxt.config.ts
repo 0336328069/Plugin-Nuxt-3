d01cc257066d06ebf90fae3239dbe03985eddb06
Hung Ho <hung.ho@mgisolutions.com>
2023-05-16 09:09:23 +0700

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    typescript: {
+        shim: false,
+    },
+    plugins: [{ src: `~/plugins/notification.client.ts`, mode: "client"}]
 })
